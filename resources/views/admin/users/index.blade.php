d6e60f9347c9d8d8a568dfeb46f1cc15e645eee4
JulesAD96 <adonsijules9@gmail.com>
2022-09-25 20:18:20 +0100

    User list update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,13 +3,34 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Liste des users') }}</div>
                 <div class="card-body">
-                    @foreach ($users as $user)
-                        {{ $user->name | $user->email }}
-                    @endforeach
+                    <table class="table table-bordered">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Actions</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <th scope="row"> {{ $user->id }} </th>
+                                    <td> {{ $user->name }} </td>
+                                    <td> {{ $user->email }} </td>
+                                    <td>
+                                        <a href="{{ route('admin.users.edit', $user->id) }}"> <button class="btn btn-primary">Editer</button> </a>
+                                        <a href="{{ route('admin.users.destroy',$user->id) }}"> <button class="btn btn-danger">Supprimer</button> </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                          
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
